0549b2a6c8ef34908a1b5f8da8280d3d57349438
lelimacon <lelimacon@users.noreply.github.com>
2026-07-13 10:54:57 +0200

    Git log formatting and cocogitto

--- home/modules/console/shell.nix
+++ home/modules/console/shell.nix
@@ -12,6 +12,10 @@
     "ll" = "eza -l --icons";
     "tree" = "eza --tree";
 
+    # Git.
+    # Git log formatting: https://gist.github.com/niun/ca61a37791ff1fdc9b33
+    "gl" = "git log --all --graph --pretty=format:'%Cgreen%ad%Creset %C(auto)%h %s %C(bold black)<%aN>%C(auto)%d%Creset' --date=format-local:'%Y-%m-%d %H:%M'";
+
     # Dev shells.
     "dev" = "develop ${toString ../../..}";
     "dev-builder" = "nix develop path:${toString ../../../shells/gtk}    --command gnome-builder";

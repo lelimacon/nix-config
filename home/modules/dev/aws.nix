6dd13325578ca639d5989e0141742ce8f007c782
lelimacon <lelimacon@users.noreply.github.com>
2026-07-13 11:57:36 +0200

    Darwin sytem update

--- home/modules/dev/aws.nix
+++ home/modules/dev/aws.nix
@@ -7,8 +7,13 @@
 {
   home.packages = with pkgs;
   [
-    pkgs-unstable.awscli2 # CLI tool.
+    pkgs-unstable.awscli2 # AWS CLI unified tool.
     ssm-session-manager-plugin
-    pkgs-unstable.aws-sam-cli # CLI tool for local development and testing
+    (pkgs-unstable.aws-sam-cli.overrideAttrs (old: {
+      disabledTests = (old.disabledTests or []) ++ [
+        # Broken with Python 3.14 in 1.163.0.
+        "test_toml_invalid_file_name"
+      ];
+    })) # CLI tool for local development and testing.
   ];
 }

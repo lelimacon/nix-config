f3cd4670c60b60cdd5327b236e87a57ba16fe628
lelimacon <lelimacon@users.noreply.github.com>
2026-08-09 11:30:40 +0200

    Boot drive fix

--- system/hosts/ff08-amd/hardware-configuration.nix
+++ system/hosts/ff08-amd/hardware-configuration.nix
@@ -19,7 +19,7 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/94CE-8039";
+    { device = "/dev/disk/by-uuid/12CE-A600";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };

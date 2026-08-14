d06f84eda42da3ff8d0072599e82958d6a5714f2
lelimacon <lelimacon@users.noreply.github.com>
2026-08-14 10:56:50 +0200

    Go shell GOROOT

--- shells/go.nix
+++ shells/go.nix
@@ -19,14 +19,18 @@ pkgs.mkShell
 
   shellHook =
   ''
-    export GO_SDK_HOME="${pkgs.go}/lib/go"
+    export GOROOT="${pkgs.go}/share/go"
 
     export GO_VERSION="$(go version | cut -c14-)"
+    export GOVULN_VERSION="$(govulncheck --version 2>&1 | awk '/^Scanner:/{split($2,a,"@"); v=a[2]} /^DB updated:/{sub(/^DB updated: /,""); print v " — " $0}')"
+    export CLANG_VERSION="$(clang --version 2>&1 | awk 'NR==1{sub(/.*version /, ""); v=$0} NR==2{sub(/Target: /, ""); print v " " $0}')"
 
     export CC="${pkgs.clang}/bin/clang"
     export CGO_CFLAGS="--sysroot=$(xcrun --show-sdk-path)"
     export CGO_LDFLAGS="--sysroot=$(xcrun --show-sdk-path)"
 
-    echo "Go  $GO_VERSION"
+    echo "Go      $GO_VERSION"
+    echo "Govuln  $GOVULN_VERSION"
+    echo "Clang   $CLANG_VERSION"
   '';
 }
